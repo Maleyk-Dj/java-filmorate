226a6febed5d339f0246369478fc7ebe27e189c0
Maleyk <m9375774872@gmail.com>
2025-06-01 14:54:03 +0300

    Вынесена логика хранения данных в отдельное хранилище/Реализованы сервисы для добавления и удаления друзей у пользователей/Добавлены сервисы для добавления и удаления фильмов/Контроллеры приведены к REST API с корректными эндпоинтами и методами/Настроено детальное логирование для всех операций

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,11 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
